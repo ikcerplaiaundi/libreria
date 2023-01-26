904fd2eade0108a6d13f08f4b7a4ec18fb17caef
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-01-26 12:24:41 +0100

    Sys o menus

--- src/libreria/Menu.java
+++ src/libreria/Menu.java
@@ -1,5 +1,36 @@
 package libreria;
 
 public class Menu {
+/*En la Fase1 el menu principal apareceran las opciones:
+   Gestionar Libros
+   Gestionar Socios
+   Gestionar Prestamos
+  En el menu libros:
+   Insertar libro
+   Eliminar libro
+   Ver libros
+  Menu socios:
+   Ver socios
+  Menu prestamos:
+   Realizar préstamo
+*/
+public static void menuprincipal(){
+	System.out.println("  1 Gestionar Libros\r\n"
+			+ "  2 Gestionar Socios\r\n"
+			+ "  3 Gestionar Prestamos");
+}
 
+public static void menuLibros(){
+	System.out.println("  1 Insertar libro\r\n"
+			+ "  2 Eliminar libro\r\n"
+			+ "  3 Ver libros");
+}
+public static void menuSocios(){
+	System.out.println(" 1 Ver socios");
+}
+public static void menuPrestamos(){
+	System.out.println(" 1 Realizar préstamo");
+}
+	
+	
 }
