2676bd44f60a9103ae7a506f7b843f97ac56e48f
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-01-31 10:01:15 +0100

    gestor libro con avisos

--- src/libreria/GestorLibros.java
+++ src/libreria/GestorLibros.java
@@ -8,6 +8,7 @@ public static void run(Scanner scan) throws SQLException {
 	int opcion;
 	GestorBBDD GBBDD=new GestorBBDD();
 	FormulariosDeDatos FDatos=new FormulariosDeDatos();
+	Visor visor=new Visor();
 	do {
 		Menu.menuLibros();
 		opcion= Integer.parseInt(scan.nextLine());
@@ -16,14 +17,16 @@ public static void run(Scanner scan) throws SQLException {
 		switch (opcion) {
 		case Menu.INSERTAR_LIBRO:
 			System.out.println("primera opcion seleccionada\n");
-			Libro libro = FDatos.pedirDatosLibro(scan);
-			GBBDD.insertarLibros(libro);
+			
+			GBBDD.insertarLibros(FDatos.pedirDatosLibro(scan));
 			break;
 		case Menu.ELIMINAR_LIBRO:
 			System.out.println("segunda opcion seleccionada\n");
+			GBBDD.eliminarLibro(FDatos.pedirIdLibro(scan));
 			break;
 		case Menu.VER_LIBROS:
 			System.out.println("tercera opcion seleccionada\n");
+			visor.mostrarLibros(null);
 			break;
 		case Menu.SALIR:
 			System.out.println("ADIOS");
